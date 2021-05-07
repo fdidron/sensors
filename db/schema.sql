6728c3f710c5a66971e613e8c3f2aaccb071cb54
Florian Didron <0x6664@hey.com>
2021-05-07 19:00:51 +0900

    chore: update sql schema/seed

--- db/schema.sql
+++ db/schema.sql
@@ -14,6 +14,6 @@ CREATE TABLE `readings`(
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `value` VARCHAR(16) NOT NULL,
   `sensor_id` INTEGER NOT NULL,
-  `created_at` DATE DEFAULT (datetime('now','localtime')),
+  `created_at` DATE DEFAULT(datetime('now','localtime')),
   FOREIGN KEY(`sensor_id`) REFERENCES sensors(`id`)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,6 @@
-INSERT INTO categories VALUES(1,'Home');
-INSERT INTO categories VALUES(2,'Garden');
-INSERT INTO sensors VALUES(1,'avocado1','Lisa''s Avocado','Percent',1);
-INSERT INTO sensors VALUES(2,'avocado2','Mama''s Avocado','Percent',1);
-INSERT INTO sensors VALUES(3,'avocado3','Papa''s Avocado','Percent',1);
+INSERT INTO categories VALUES(1,'Plants');
+INSERT INTO categories VALUES(2,'Home');
+INSERT INTO categories VALUES(3,'Garden');
+INSERT INTO sensors VALUES(1,'avocado1','Lisa''s Avocado','Moisture',1);
+INSERT INTO sensors VALUES(2,'avocado2','Mayumi''s avocado','Moisture',1);
+INSERT INTO sensors VALUES(3,'avocado3','Flo''s avocado','Moisture',1);
